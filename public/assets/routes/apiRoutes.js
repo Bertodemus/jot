f87b2c16e1cd3de2d690c42effd36fde127fe237
Bertodemus <roberto@robertorubet.com>
2020-11-01 22:11:26 -0500

    updated formatting

--- public/assets/routes/apiRoutes.js
+++ public/assets/routes/apiRoutes.js
@@ -1,9 +1,15 @@
-// const notes = require("../../../db/db.json");
+// ==============================================================================
+// DEPENDENCIES
+// ==============================================================================
 var fs = require('fs');
 var uuid = require("uuid");
 
+// ==============================================================================
+// ROUTES FOR API
+// ==============================================================================
 module.exports = function(app) {
 
+  //GET method
   app.get("/api/notes", function(req, res) {
     fs.readFile("./db/db.json", (err, data) => {
      var parsedNote = JSON.parse(data);
@@ -11,6 +17,7 @@ module.exports = function(app) {
     });
   });
 
+  //POST method
   app.post("/api/notes", function(req, res) {
     const newNote = {
       title: req.body.title,
@@ -18,61 +25,27 @@ module.exports = function(app) {
       id: uuid.v4()
     }
 
-      fs.readFile("./db/db.json", (err, data) => {
-        var parsedNote = JSON.parse(data);
-
-        
-        parsedNote.push(newNote);
-
-          fs.writeFile("./db/db.json", JSON.stringify(parsedNote), function(err, result) {
-            if(err) console.log('error', err);
-          });
-        });
-    res.send("updated");
-  });
-
-
-
-  app.delete("/api/notes/:id", function(req, res) {
-
     fs.readFile("./db/db.json", (err, data) => {
       var parsedNote = JSON.parse(data);
-      
-
-     var newNote = parsedNote.filter(note => note.id !== req.params.id);
-
-    fs.writeFile("./db/db.json", JSON.stringify(newNote), function(err, result) {
-      if(err) console.log('error', err);
+      parsedNote.push(newNote);
+      fs.writeFile("./db/db.json", JSON.stringify(parsedNote), function(err, result) {
+        if(err) console.log('error', err);
+      });
     });
-  });
     res.send("updated");
   });
 
-};
 
+  //DELETE method
+  app.delete("/api/notes/:id", function(req, res) {
+    fs.readFile("./db/db.json", (err, data) => {
+      var parsedNote = JSON.parse(data);
+      var newNote = parsedNote.filter(note => note.id !== req.params.id);
+      fs.writeFile("./db/db.json", JSON.stringify(newNote), function(err, result) {
+        if(err) console.log('error', err);
+      });
+    });
+    res.send("updated");
+  });
 
-// app.delete("/api/notes/:id", function(req, res){
-    
-//   var uniqueId = req.params.id;
-
-//   fs.readFile("Develop/db/db.json", (err, data) => {
-
-//     var parsed = JSON.parse(data)
-//     var remove = parsed.filter(note => note.id !== uniqueId)
-//     fs.writeFile("Develop/db/db.json", JSON.stringify(remove), err => {
-//       if(err) throw err;
-//       res.send("Note deleted!");
-//     })
-
-//   })
- 
-// }); 
-// Tanner Kirkpatrick12:57 PM
-// app.get("/api/notes", function(req, res) {
-// fs.readFile("Develop/db/db.json", (err, data) => {
-//   if (err){
-//     throw err;
-//   }
-//   res.json(JSON.parse(data));
-// })
-// });
+}
